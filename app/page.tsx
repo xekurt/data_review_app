5adbfb3a21cbebc67a40c8e3b2436ce78c8b86fc
Ayoub Rezaei <ayoubrezaei79@gmail.com>
2025-11-26 23:33:20 +0100

    refactor Home component layout and integrate Processes, Subprocesses, and Tasks

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
+"use client";
+
 import Header from "./_src/components/header";
+import Processes from "./_src/components/Processes";
+import Subprocesses from "./_src/components/Subprocesses";
+import Tasks from "./_src/components/Tasks";
+import { useInitializeProcesses } from "./_src/hooks/useProcessData";
 
 export default function Home() {
+  useInitializeProcesses();
+
   return (
-    <section>
+    <section className="h-screen flex flex-col">
       <Header />
+      <div className="flex flex-1 overflow-hidden">
+        <div className="flex-1 overflow-auto border-r border-gray-200 dark:border-gray-700">
+          <Processes />
+        </div>
+        <div className="flex-1 overflow-auto border-r border-gray-200 dark:border-gray-700">
+          <Subprocesses />
+        </div>
+        <div className="flex-1 overflow-auto">
+          <Tasks />
+        </div>
+      </div>
     </section>
   );
 }
